4fd42ecbb5e2dee7a7a09f79fbf178a852b97f67
bws9000 <snyder.burt@gmail.com>
2026-05-15 12:51:20 -0400

    Update gemini config

--- src/examples/relay-ops/gemini/gemini.service.deprecated.ts
+++ src/examples/relay-ops/gemini/gemini.service.deprecated.ts
@@ -3,17 +3,13 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 
 @Injectable()
 export class GeminiService {
-
-  private readonly genAI = new GoogleGenerativeAI(
-    process.env.GEMINI_API_KEY!
-  );
+  private readonly genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
   private readonly model = this.genAI.getGenerativeModel({
     model: 'gemini-1.5-flash',
   });
 
   async prompt(prompt: string): Promise<string> {
-
     const result = await this.model.generateContent(prompt);
 
     return result.response.text();
